bf7f7e8f64e91b42fbe33dfb0d936dcee585501a
wang1629 <apprentice89@126.com>
2018-11-01 11:04:59 +0800

    add python_http_server

---
+++ python_http_server/start_web_server.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import SimpleHTTPServer
+import SocketServer
+import sys
+
+PORT = 6789
+if len(sys.argv) > 1:
+  PORT=int(sys.argv[1])
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+print "serving at port", PORT
+httpd.serve_forever()
